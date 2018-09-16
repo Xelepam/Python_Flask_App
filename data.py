8394d820cb6a6939888f2cbb4a2a7e4c87934587
Omar Enrique Castro <oecastrom@gmail.com>
2018-09-15 21:23:15 -0500

    further developed endpoints

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident reprehenderit suscipit beatae. Ducimus quas, laboriosam quia quasi exercitationem minus provident architecto eligendi quisquam omnis minima molestiae, possimus sunt, aliquid quos?',
+            'author': 'Omar E. Castro',
+            'create_date': '09-15-2018'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident reprehenderit suscipit beatae. Ducimus quas, laboriosam quia quasi exercitationem minus provident architecto eligendi quisquam omnis minima molestiae, possimus sunt, aliquid quos?',
+            'author': 'John Doe',
+            'create_date': '09-15-2018'   
+        },
+         {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident reprehenderit suscipit beatae. Ducimus quas, laboriosam quia quasi exercitationem minus provident architecto eligendi quisquam omnis minima molestiae, possimus sunt, aliquid quos?',
+            'author': 'Samuel Smith',
+            'create_date': '09-15-2018'   
+        }
+    ]
+    return articles
